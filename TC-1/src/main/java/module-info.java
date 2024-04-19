055a6c289a4b14c640b1fe64a347cf5b261518fe
gnahraf <farhang@gmail.com>
2024-04-19 04:01:05 -0600

    Legacy time chain moved to TC-1. Its succesor knows nothing about this code. POM coordinates remain the same, but module-info name has changed to 'io.crums.legacy.tc1'

--- TC-1/src/main/java/module-info.java
+++ TC-1/src/main/java/module-info.java
@@ -1,9 +1,12 @@
-module io.crums.core {
+module io.crums.legacy.tc {
+  
   requires transitive io.crums.util.mrkl;
   requires transitive io.crums.jsonimple;
+  
   requires io.crums.io.store;
   requires java.net.http;
   
+  // following to be purged after TC-2 redesign..
   exports io.crums.client;
   exports io.crums.client.repo;
   exports io.crums.model;
