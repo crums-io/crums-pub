3334700e917b179249fededd6ca39e93479fb496
Babak <babak@crums.io>
2024-05-11 18:09:03 -0600

    gpg-sign under profile only ..and another javadoc build fix

--- ergd/src/main/java/io/crums/tc/notary/server/package-info.java
+++ ergd/src/main/java/io/crums/tc/notary/server/package-info.java
@@ -6,7 +6,7 @@
  * We use virtual threads as a quick way to non-blocking I/O
  * (try this first before using Netty).
  * <em>Package not exported in module-info.</em>
- * <h3>Side Note</h3>
+ * <h2>Side Note</h2>
  * <p>
  * A few of the helper classes defined here are
  * reusable and might deserve an independent home.
